dac86be09d0b675f55978bb38e6b91a2b31d7a4a
superman <raphael.muenster@math.tu-dortmund.de>
2017-03-29 09:13:23 +0200

    Added new application for spring bodies

---
+++ applications/q2p1_fbm_spring/defs_include.h
@@ -0,0 +1,14 @@
+
+  USE def_FEAT
+  USE PLinScalar, ONLY : Init_PLinScalar,InitCond_PLinLS, &
+    UpdateAuxVariables,Transport_PLinLS,Reinitialize_PLinLS, &
+    Reinit_Interphase,dMaxSTF
+  USE Transport_Q2P1, ONLY : Init_QuadScalar_Stuctures, &
+    InitCond_QuadScalar,Transport_q2p1_UxyzP,ProlongateSolution, &
+    ResetTimer,bTracer,bViscoElastic,StaticMeshAdaptation,Transport_q2p1_UxyzP_fc_ext
+  USE ViscoScalar, ONLY : Init_ViscoScalar_Stuctures, &
+    Transport_ViscoScalar,IniProf_ViscoScalar,ProlongateViscoSolution
+  USE Transport_Q1, ONLY : Init_LinScalar,InitCond_LinScalar, &
+    Transport_LinScalar
+  USE PP3D_MPI, ONLY : myid,master,showid,myMPI_Barrier
+  USE var_QuadScalar, ONLY : myStat,cFBM_File
