5d233ae0a304c3909e0971fb0b4de1739b8bb5fb
Raphael Muenster <raphael.muenster@gmail.com>
2017-10-30 13:24:40 +0100

    WIP: added options to python combine tool

--- tools/repartition.py
+++ tools/repartition.py
@@ -4,16 +4,17 @@
 import os
 import sys
 import re
+import getopt
 
-# Scan what is in the _dump/processor_X folder 
+# Scan what is in the _dump/processor_X folder
 # for each .dmp field found there
 # read and join the partitioned files to
-# a single file per field 
+# a single file per field
 
-#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-   
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 def cmp_entries(a, b):
-# sort the tuples in the list element_entries 
+# sort the tuples in the list element_entries
 # according to the global element index
   if a[0] > b[0]:
     return 1
@@ -22,11 +23,11 @@ def cmp_entries(a, b):
   else:
     return -1
 
-#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-   
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 def readPartitionedField(elem_entries, fileName):
 # reads a single field from the file <fileName>
-# and stores it in elem_entries         
+# and stores it in elem_entries
 
   header_info = {}
   header_line = ""
@@ -34,11 +35,11 @@ def readPartitionedField(elem_entries, fileName):
   with open(fileName, "r") as f:
     header = f.readline()
     header_line = header
-    print(header)
+    #print(header)
     split_header = header.strip().split(",")
-    print(split_header)
+    #print(split_header)
     header_info = {(pair[0]) : (pair[1]) for pair in (l.split(":") for l in split_header)}
-    print(header_info['Components'])
+    #print(header_info['Components'])
 
     while True:
       line = f.readline()
@@ -58,11 +59,11 @@ def readPartitionedField(elem_entries, fileName):
 
       elem_entries.append(entry)
 
-    return header_line
+    return header_line, header_info
 
-#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-   
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-def writeMergedField(elem_entries, header, components, fileName):
+def writeCombinedField(elem_entries, header, components, fileName):
   with open(fileName, "w") as f:
     f.write(header)
     for e in elem_entries:
@@ -70,41 +71,89 @@ def writeMergedField(elem_entries, header, components, fileName):
       for i in range(components):
         f.write(" ".join(e[1][i]) + '\n')
 
-#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-   
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
+def combineField(nprocs,fieldName, path, out_idx):
 
+  element_entries = []
 
-#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-   
-        
+  for i in range(1,nprocs+1):
+    fileToRead = path + "/processor_" + str(i) + "/" + str(out_idx) + "/" + fieldName
+
+    header_line, header_info = readPartitionedField(element_entries, fileToRead)
+
+    element_entries.sort(cmp_entries)
+
+    print("Number of elements entries: " + str(len(element_entries)))
+
+    writeCombinedField(element_entries, header_line, int(header_info['Components']), fieldName)
+
+  print(fieldName + "\n" + header_info['Components'] + "\n" + header_line)
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+def getDumpFields(nprocs, path, out_idx):
+
+  print("Found " + str(nprocs) + " processor_ directories")
+
+  print("Dump directory: " + str(dir_list))
+
+  print("Number of processors: " + str(num_processors))
+
+  dump_path = path + "/processor_" + str(1) + "/" + str(out_idx)
+
+  print("Dump load path: " + dump_path)
+
+  potential_files = os.listdir(dump_path)
+
+  files = []
+
+  for item in potential_files:
+    m = re.search(r"\w+.dmp", item)
+    if m and item != "time.dmp":
+      files.append(item)
+
+  return files
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+def processDumpDir(nprocs, path, out_idx):
+
+  print("Found " + str(nprocs) + " processor_ directories")
+
+  print("Dump directory: " + str(dir_list))
+
+  print("Number of processors: " + str(num_processors))
+
+  dump_path = dump_folder_path + "/processor_" + str(1) + "/" + str(dump_folder_idx)
+
+  print("Dump load path: " + dump_path)
+
+  print("Backup files: " + str(os.listdir(dump_path)))
+
+  header_line = ""
+  element_entries = []
+
+  fields = getDumpFields(nprocs, path, out_idx)
+
+  for f in fields:
+    combineField(num_processors, f, dump_folder_path, dump_folder_idx)
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+def usage():
+  print("Usage: python combine_fields.py [options]")
+  print("Where options can be:")
+  print("[-h, --help]: prints this message")
+  print("[-d, --dump-path]: path to the dump folder")
+  print("[-i, --idx]: index of the dump folder")
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
 # Parameters for reading and merging the field:
-# Number of processors: 
+# Number of processors:
 # - equal to number of processor_* directories in the folder
-# - the dump folder index 
-
-dir_list = os.listdir("./_dump")
-
-dump_folder_idx = 1
-
-step = dump_folder_idx
-
-num_processors = 0
-
-for e in dir_list:
-  m = re.search(r"processor_\d+", e)
-  if m:
-    num_processors = num_processors + 1
-
-print("Found " + str(num_processors) + " processor_ directories")
-
-print("Dump directory: " + str(dir_list))
-
-print("Number of processors: " + str(num_processors))
-
-dump_path = "./_dump/processor_" + str(1) + "/" + str(step)
-
-print("Dump load path: " + dump_path)
-
-print("Backup files: " + str(os.listdir(dump_path)))
+# - the dump folder index
 
 # The format of a .dmp file is as follows:
 # - it is a list of coarse element entries
@@ -113,18 +162,40 @@ print("Backup files: " + str(os.listdir(dump_path)))
 # - after the first line there is a line for each component of the output field
 # - in each 'component line' the values of fine mesh dofs in the coarse element are given in the FEATFLOW multi-level ordering
 
-header_line = ""
-element_entries = []
+# Script begin
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-#for i in range(1,5):
-#  fileToRead = "./_dump/processor_" + str(i) + "/" + str(step) + "/" + "velocity.dmp"
-#  header_line = readPartitionedField(element_entries, fileToRead)
-#
-#    #print("Using directories: " + "./_dump/processor_" + str(i) + "/" + str(step))
-#
-#element_entries.sort(cmp_entries)
-#
-#print("Number of elements entries: " + str(len(element_entries)))
-#    
-#writeMergedField(element_entries, header_line, header_line['Components'], "velocity.dmp"):
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'i:d:h', ['idx=','dump-path=', 'help'])
+except getopt.GetoptError:
+    usage()
+    sys.exit(2)
 
+dump_folder_idx = 1
+
+dump_folder_path = "./_dump"
+
+for opt, arg in opts:
+    if opt in ('-h', '--help'):
+        usage()
+        sys.exit(2)
+    elif opt in ('-i', '--idx'):
+        dump_folder_idx = arg
+    elif opt in ('-d', '--dump-path'):
+        dump_folder_idx = arg
+#    elif opt in ('-p', '--params'):
+#        params = arg
+    else:
+        usage()
+        sys.exit(2)
+
+dir_list = os.listdir(dump_folder_path)
+
+num_processors = 0
+
+for e in dir_list:
+  m = re.search(r"processor_\d+", e)
+  if m:
+    num_processors = num_processors + 1
+
+processDumpDir(num_processors, dump_folder_path, dump_folder_idx)
