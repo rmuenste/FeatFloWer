7c194c16be7dd0c2d6a02185b830df9b95fe5c98
superman <raphael.muenster@math.tu-dortmund.de>
2018-03-06 11:20:44 +0100

    Added files for sse application

---
+++ applications/q2p1_sse/q2p1_sse.f90
@@ -0,0 +1,63 @@
+PROGRAM Q2P1_SSE
+
+  include 'defs_include.h'
+
+  use solution_io, only: postprocessing_sse
+
+  use post_utils,  only: handle_statistics,&
+                         print_time,&
+                         sim_finalize
+
+  use Transport_q2p1, only : Transport_q2p1_UxyzP_sse
+
+  integer            :: iOGMV,iTout
+  character(len=200) :: command
+  character(len=60)  :: CPP3D
+  real               :: dout = 0.0
+  integer            :: ufile,ilog
+  real               :: tt0 = 0.0
+  real               :: dtt0 = 0.0
+
+  !-------INIT PHASE-------
+
+  call init_q2p1_ext(ufile)
+
+  CALL ZTIME(tt0)
+  call ztime(dtt0)
+
+  dout = Real(INT(timens/dtgmv)+1)*dtgmv
+
+  !-------MAIN LOOP-------
+
+  DO itns=1,nitns
+
+  itnsr=0
+  timnsh=timens
+  dt=tstep
+  timens=timens+dt
+
+  ! Solve Navier-Stokes (add discretization in name + equation or quantity)
+  call Transport_q2p1_UxyzP_sse(ufile,inl_u, itns)
+
+  IF (bTracer) THEN
+    ! Solve transport equation for linear scalar
+    CALL Transport_LinScalar(ufile,inonln_t)
+  ELSE
+    inonln_t = 2
+  END IF
+
+  call postprocessing_sse(dout, inonln_u, inonln_t,ufile)
+
+  call print_time(timens, timemx, tstep, itns, nitns, ufile, uterm)
+
+  call handle_statistics(tt0,itns)
+
+  istep_ns = istep_ns + 1
+  ! Exit if done
+  IF (timemx.LE.(timens+1D-10)) EXIT
+
+  END DO
+
+  call sim_finalize(tt0,ufile)
+
+END PROGRAM Q2P1_SSE
