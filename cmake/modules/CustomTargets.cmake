899dda79ed98ef1a0a0fa4f3b1a2d7d8fc699782
Raphael Muenster <raphael.muenster@gmail.com>
2019-03-01 13:08:48 +0100

    Added custom target to CMakeLists for Build ID output

--- cmake/modules/CustomTargets.cmake
+++ cmake/modules/CustomTargets.cmake
@@ -1,10 +1,11 @@
 #=========================================================================
 #                         custom commands
 #=========================================================================
-set(CUSTOM_DEPENDENCY "my_file.stamp")
+set(CUSTOM_DEPENDENCY "BuildID-Output")
 add_custom_command(
-    COMMAND ${CMAKE_COMMAND} -E touch ${CUSTOM_DEPENDENCY} 
+#    COMMAND ${CMAKE_COMMAND} -E touch ${CUSTOM_DEPENDENCY} 
+    COMMAND python ${CMAKE_SOURCE_DIR}/tools/cmake-py/get_build_ids.py 
     OUTPUT ${CUSTOM_DEPENDENCY}
 )
 
-add_custom_target(custom-target DEPENDS ${CUSTOM_DEPENDENCY})
+add_custom_target(output-build-ids DEPENDS ${CUSTOM_DEPENDENCY})
