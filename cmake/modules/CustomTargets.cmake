b26061a8e5d6626400e185bd800eb66d46395930
superman <raphael.muenster@math.tu-dortmund.de>
2019-08-09 13:33:07 +0200

    Changed the installation directory of the cgal library

--- cmake/modules/CustomTargets.cmake
+++ cmake/modules/CustomTargets.cmake
@@ -1,14 +1,28 @@
 #=========================================================================
 #                         custom commands
 #=========================================================================
-set(CUSTOM_DEPENDENCY "BuildID-Output")
-add_custom_command(
-#    COMMAND ${CMAKE_COMMAND} -E touch ${CUSTOM_DEPENDENCY} 
-    COMMAND python ${CMAKE_SOURCE_DIR}/tools/cmake-py/get_build_ids.py 
-    OUTPUT ${CUSTOM_DEPENDENCY}
-)
+#set(CUSTOM_DEPENDENCY "BuildID-Output")
+#add_custom_command(
+##    COMMAND ${CMAKE_COMMAND} -E touch ${CUSTOM_DEPENDENCY} 
+#    COMMAND python ${CMAKE_SOURCE_DIR}/tools/cmake-py/get_build_ids.py 
+#    OUTPUT ${CUSTOM_DEPENDENCY}
+#)
+#
+#add_custom_target(output-build-ids DEPENDS ${CUSTOM_DEPENDENCY})
 
-add_custom_target(output-build-ids DEPENDS ${CUSTOM_DEPENDENCY})
+#=========================================================================
+#           A function that adds a custom command to a target 
+#=========================================================================
+function(addCompilerInfo targetName)
+
+  add_custom_command(
+    TARGET ${targetName}
+    POST_BUILD
+    COMMAND module list |& cat > compiler-info.txt 
+    COMMENT "Generating compiler-info"
+  )
+
+endfunction(addCompilerInfo targetName)
 
 #=========================================================================
 #                         UnZip custom target
@@ -21,3 +35,12 @@ add_custom_target(output-build-ids DEPENDS ${CUSTOM_DEPENDENCY})
 #DEPENDS ${CMAKE_SOURCE_DIR}/abc.zip
 #COMMENT "Unpacking abc.zip"
 #VERBATIM)
+
+
+#set(CUSTOM_DEPENDENCY "compiler-info.txt")
+#
+#add_custom_command(
+#  COMMAND module list |& cat > compiler-info.txt 
+#  COMMENT "Generating compiler-info"
+#  OUTPUT ${CUSTOM_DEPENDENCY}
+#)
