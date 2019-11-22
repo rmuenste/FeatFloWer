0003742866fb649a5eab8e0b548c47e7d541cfc9
superman <raphael.muenster@math.tu-dortmund.de>
2019-11-22 14:41:44 +0100

    Updated the PyPartitioner.py script for the current package organization. The last 2 commits may force you to update your environment variables if you are experiencing issues with partitioning

--- tools/PyPartitioner.py
+++ tools/PyPartitioner.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from part_main import *
+import partitioner
 
 
 # Erzeuge benötigte Verzeichnisse, falls noch nicht vorhanden
-mkdir("_mesh")
+partitioner.mkdir("_mesh")
 
-NPart, PartMethod, NSubPart, MeshName, ProjektFile = checkParameters(sys.argv) 
+NPart, PartMethod, NSubPart, MeshName, ProjektFile = partitioner.checkParameters(sys.argv) 
 
 # Aufruf der Hauptroutine
-MainProcess(NPart,PartMethod,NSubPart,MeshName,ProjektFile)
+partitioner.MainProcess(NPart,PartMethod,NSubPart,MeshName,ProjektFile)
