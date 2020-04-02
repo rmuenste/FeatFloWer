52d0c4b8c6f7e2497a50c1feeb13c0f71574dd12
Otto Mierka <omierka@math.tu-dortmund.de>
2020-04-02 21:55:32 +0200

    heat_start.py

--- applications/heat/heat_start.py
+++ applications/heat/heat_start.py
@@ -7,6 +7,7 @@ from tempfile import mkstemp
 from shutil import move
 from os import fdopen, remove
 import os
+import shutil
 import sys
 sys.path.append(os.environ['FF_PY_HOME'])
 import partitioner
@@ -27,7 +28,7 @@ def usage():
     print("Where options can be:")
     print("[-h, --help]: prints this message")
     print("[-n, --num-processors]: defines the number of parallel jobs to be used")
-    print("[-i, --in-file]: defines the input project file (will be replaced in the q2p1-data-file)")
+    print("[-f, --in-folder]: defines the input project file (will be replaced in the q2p1-data-file)")
 
 def replace(file_path, pattern, subst):
     #Create temp file
@@ -46,13 +47,15 @@ def replace(file_path, pattern, subst):
     move(abs_path, file_path)
 
 def main(argv):
-    inputFile = ''
+    inputFile = '_data/meshDir/file.prj'
+    inputCaseFolder = ''
+    inputCaseFile = ''
     outputFile = ''
 
     numProcessors = -1
 
     try:
-        opts, args = getopt.getopt(argv, "hi:n:", ["in-file=", "num-processors="])
+        opts, args = getopt.getopt(argv, "hf:n:", ["in-folder=", "num-processors="])
     except getopt.GetoptError:
         sys.exit(2)
     for opt, arg in opts:
@@ -61,15 +64,24 @@ def main(argv):
             sys.exit()
         elif opt in ("-n", "--num-processors"):
             numProcessors = int(arg)
-        elif opt in ("-i", "--in-file"):
-            inputFile = arg
+        elif opt in ("-f", "--in-folder"):
+            inputCaseFolder = arg
 
     print('Number Of Processors is: '+ str(numProcessors))
-    print('Input file is '+ inputFile)
+    
+    inputCaseFile = inputCaseFolder+'/heat.s3d'
+    print('Input case file is '+ inputCaseFile)
+    
+    shutil.copyfile(inputCaseFile, "_data/heat.s3d")
 
-    replace("_data/q2p1_param.dat",
-            "SimPar@ProjectFile = ",
-            "SimPar@ProjectFile = '" + inputFile + "'")
+    #replace("_data/q2p1_param.dat",
+            #"SimPar@ProjectFile = ",
+            #"SimPar@ProjectFile = '" + inputFile + "'")
+
+    if os.path.exists("_data/meshDir"):
+      shutil.rmtree("_data/meshDir")
+
+    subprocess.call(['./s3d_mesher -a %s -a %s' %('heat', 'heat')], shell=True)
 
     partitioner.partition(numProcessors - 1, 1, 1, "NEWFAC", str(inputFile))
     subprocess.call(['mpirun -np %i ./%s' %(numProcessors, 'heat')], shell=True)
