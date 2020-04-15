22bf02e0208e6dea66b2373f3ab66711ed07936f
Dirk Ribbrock <dirk.ribbrock@math.tu-dortmund.de>
2020-04-15 14:20:08 +0200

    conv_check.sh: move plot_it.gp to _temp dir

--- applications/q2p1_sse/conv_check.sh
+++ applications/q2p1_sse/conv_check.sh
@@ -14,7 +14,7 @@ do
  grep ressureDiff $i > _temp/${j}.pres
  grep acting $i > _temp/${j}.torque
  t="$t '_temp/${j}.pres' u 5 w l notitle," 
- s="$s '_temp/${j}.torque' u 4 w l notitle," 
+ s="$s '_temp/${j}.torque' u 5 w l notitle,"
 # echo ${t}
 done
 
@@ -23,24 +23,22 @@ s="$s 0.0 w l lc rgb 'black'  notitle"
 echo ${t}
 echo ${s}
 
-echo 'set terminal png size 1600,1200 ' > plot_it.gp
-echo 'set output "convergence.png" ' >> plot_it.gp
+echo 'set terminal png size 1600,1200 ' > _temp/plot_it.gp
+echo 'set output "convergence.png" ' >> _temp/plot_it.gp
 
-echo 'set multiplot layout 2, 1 title "Convergence" font ",14"'  >> plot_it.gp
+echo 'set multiplot layout 2, 1 title "Convergence" font ",14"'  >> _temp/plot_it.gp
 
-echo 'set title "Pressure"'  >> plot_it.gp
-echo ${t} >> plot_it.gp
+echo 'set title "Pressure"'  >> _temp/plot_it.gp
+echo ${t} >> _temp/plot_it.gp
 
-echo " " >> plot_it.gp
+echo " " >> _temp/plot_it.gp
 
-echo 'set title "Power"'  >> plot_it.gp
-echo ${s} >> plot_it.gp
+echo 'set title "Power"'  >> _temp/plot_it.gp
+echo ${s} >> _temp/plot_it.gp
 
-echo 'unset multiplot ' >> plot_it.gp
+echo 'unset multiplot ' >> _temp/plot_it.gp
 
-gnuplot plot_it.gp
+gnuplot _temp/plot_it.gp
 rm -fr _temp
-
-#eog convergence.png
 
 exit 0
