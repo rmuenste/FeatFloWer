34d008ebc0d5e83ebdf0dde849715f2d448a87d2
Raphael Muenster <raphael.muenster@gmail.com>
2020-07-03 14:31:19 +0200

    WIP: First version of non-manifold preprocessing step with blender

--- tools/check_off/blender_check_off.py
+++ tools/check_off/blender_check_off.py
@@ -1,3 +1,39 @@
-command = "/home/raphael/bin/blender-2.79b-linux-glibc219-x86_64/blender --background -P extrude.py " \
-          "-- %s %s" \
-          "%s %f" %(blenderBase, surfaceMeshName, outputLayerName, thickness)
+import os
+import subprocess
+import getopt
+import sys
+
+def usage():
+  print("Usage: check_off.py [options]")
+  print("Where options can be:")
+  print("[-h, --help]: prints this message")
+  print("[-p, --blender-path]: path to blender executable")
+  print("[-f, --file]: the off file to check")
+
+def main():
+    blenderPath = "/home/raphael/bin/blender-2.79b-linux-glibc219-x86_64/blender" 
+    fileName = "open_cylinder.off"
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hf:p:', ['help', 'file=', 'blender-path='])
+    except getopt.GetoptError:
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            usage()
+            sys.exit()
+        elif opt in ("-p", "--blender-path"):
+            blenderPath = arg
+        elif opt in ("-f", "--file"):
+            fileName = arg
+        else:
+            usage()
+            sys.exit(2)
+
+    command = "%s --background -P check_off.py " \
+            "-- %s" %(blenderPath, fileName)
+
+    subprocess.call([command], shell=True)
+
+if __name__ == "__main__":
+    main()
--- tools/check_off/check_off.py
+++ tools/check_off/check_off.py
@@ -1,31 +1,40 @@
 import bpy,os,re
+import sys
 import mathutils
 import bmesh
 
-def checkManifold():
+
+def checkManifold(fileName):
+  objectName = os.path.basename(fileName)
+  objectName = os.path.splitext(objectName)[0]
+
   count = 0    
   myObjects = bpy.data.objects
   myScene = bpy.data.scenes['Scene']
   for o in myObjects:
-    if o.type == 'MESH':
+#    if o.type == 'MESH':
+    if re.search(objectName, o.name):
         print(o.name)                
         myScene = bpy.data.scenes['Scene']
         bpy.context.scene.objects.active = o
         o.select = True
         bpy.ops.object.mode_set(mode='EDIT')
-        #bpy.ops.mesh.select_all(action='SELECT')        
+        bpy.ops.mesh.select_all(action='TOGGLE')        
         bpy.ops.mesh.select_non_manifold() 
         bm = bmesh.from_edit_mesh(o.data)
         verts = [v.index for v in bm.verts if v.select]        
         edges = [e.index for e in bm.edges if e.select]                
         
-        
+        print("There are %s non-manifold vertices." % len(verts))
+        print("There are %s non-manifold edges." % len(edges))                                
         if len(verts) > 0 or len(edges) > 0:
-            print("There are %s non-manifold vertices." % len(verts))
-            print("There are %s non-manifold edges." % len(edges))                                
+            print("This is a non-manifold mesh.")
 
 def main():
-  checkManifold()            
+  fileName = sys.argv[5]
+  print("Name : %s" %fileName)
+  bpy.ops.import_mesh.off(filepath=fileName)
+  checkManifold(fileName)            
       
 if __name__ == "__main__":
   main()  
