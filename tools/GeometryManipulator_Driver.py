12391aacea87139795987e1b5855713f2bd297c0
Superman <raphael.muenster@math.tu-dortmund.de>
2020-07-03 11:46:47 +0200

    Quick Python3 adjustments for GeometryManipulator related files

--- tools/GeometryManipulator_Driver.py
+++ tools/GeometryManipulator_Driver.py
@@ -11,25 +11,29 @@ def usage():
   print("[-n, --num-processors]: defines the number of parallel jobs to be used")
   print("[-f, --in-folder]: defines the input project file (will be replaced in the q2p1-data-file)")
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:], 'hf:t:', ['file=', 'translation='])
-except getopt.GetoptError:
-    sys.exit(2)
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hf:t:', ['help','file=', 'translation='])
+    except getopt.GetoptError:
+        sys.exit(2)
 
-for opt, arg in opts:
-    if opt == '-h':
-        usage()
-        sys.exit()
-    elif opt in ("-t", "--translation"):
-       dZ = float(arg)
-    elif opt in ("-f", "--file"):
-        myfile = arg
-
-
-myGeo = gm.GeometryObject()
-myGeo.ReadOff('Gitter/' + myfile)
-myGeo.TranslateObject(0.0,0.0,12.5)
-myGeo.ExportOff('GitterTranslated/' + myfile)
+    for opt, arg in opts:
+        if opt == '-h':
+            usage()
+            sys.exit()
+        elif opt in ("-t", "--translation"):
+            dZ = float(arg)
+            print(dZ)
+        elif opt in ("-f", "--file"):
+            myfile = arg
+            print(myfile)
 
 
+    myGeo = gm.GeometryObject()
+    myGeo.ReadOff('Gitter/' + myfile)
+    myGeo.TranslateObject(0.0,0.0,12.5)
+    myGeo.ExportOff('GitterTranslated/' + myfile)
 
+
+if __name__ == "__main__":
+    main()
