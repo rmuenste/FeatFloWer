ac3bc72478264a6f194976432a8655794eb87f57
Otto Mierka <omierka@math.tu-dortmund.de>
2020-11-17 16:35:19 +0100

    GeometryManipulator_Driver.py extended to scale and transform ply, off, obj meshes

--- tools/GeometryManipulator_Driver.py
+++ tools/GeometryManipulator_Driver.py
@@ -15,11 +15,12 @@ def usage():
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'hf:t:', ['help','file=', 'translation='])
+        opts, args = getopt.getopt(sys.argv[1:], 'hf:s:t:', ['help','file=', 'scale=','translation='])
     except getopt.GetoptError:
         sys.exit(2)
 
     dZ=0.0
+    dS=1.0
     for opt, arg in opts:
         if opt == '-h':
             usage()
@@ -27,15 +28,34 @@ def main():
         elif opt in ("-t", "--translation"):
             dZ = float(arg)
 #            print(dZ)
+        elif opt in ("-s", "--scale"):
+            dS = float(arg)
+#            print(dZ)
         elif opt in ("-f", "--file"):
             myfile = arg
 #            print(myfile)
 
+
+    shortfilename = os.path.basename(myfile)
+    #print(shortfilename)
+    filename, file_extension = os.path.splitext(shortfilename)
+    path = myfile.replace(shortfilename,"")
+ 
     print(myfile + " is translated by "  +str(dZ))
     myGeo = gm.GeometryObject()
-    myGeo.ReadOff('Gitter/' + myfile)
+    if file_extension == '.off':
+      myGeo.ReadOff(myfile)
+    if file_extension == '.obj':
+      myGeo.ReadObj(myfile)
+    if file_extension == '.ply':
+      myGeo.ReadPly(myfile)
+      
     myGeo.TranslateObject(0.0,0.0,dZ)
-    myGeo.ExportOff('GitterTranslated/' + myfile)
+    myGeo.ScaleObject(1.0,1.0,dS)
+    
+    outfile = path + filename + "_Modified.off"
+    print(outfile)
+    myGeo.ExportOff(outfile)
 
 
 if __name__ == "__main__":
