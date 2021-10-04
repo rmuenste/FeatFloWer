eda15e92c2a5371e0ea82e2abd4e305296ad3450
Otto Mierka <omierka@math.tu-dortmund.de>
2021-10-04 08:39:01 +0200

    multimat app // basic runner.sh is added

--- applications/q2p1_sse_multimat/runner.sh
+++ applications/q2p1_sse_multimat/runner.sh
@@ -1,9 +1,10 @@
 cp _data_BU/q2p1_paramV_DIE_0.dat _data_BU/q2p1_paramV_DIE.dat
-python3 ./e3d_start.py -n 60 -f e3d_input --die-simulation
+python3 ./e3d_start.py -n 32 -f RH_LOC --die-simulation
 
 cp _data_BU/q2p1_paramAlpha.dat _data/q2p1_param.dat
 cp _data_BU/mesh_names.offs .
-mpirun -np 60 q1_scalar 
+mpirun -np 32 q1_scalar 
 
 cp _data_BU/q2p1_paramV_DIE_1.dat _data_BU/q2p1_paramV_DIE.dat
-python3 ./e3d_start.py -n 60 -f e3d_input --die-simulation
+python3 ./e3d_start.py -n 32 -f RH_LOC --die-simulation
+
