db0c947510222baebc5e7dd6eae5900cfb08e3e1
Otto Mierka <omierka@math.tu-dortmund.de>
2023-01-11 15:59:31 +0100

    shearrate averaging is activated for the fc_ext app

--- source/src_quadLS/QuadSc_transport_extensions.f90
+++ source/src_quadLS/QuadSc_transport_extensions.f90
@@ -1528,6 +1528,8 @@ IF (bNS_Stabilization) THEN
  CALL ExtractVeloGradients()
 END IF
 
+CALL IntegrateShearrate()
+                        
 call fbm_updateFBM(Properties%Density(1),tstep,timens,&
                    Properties%Gravity,mfile,myid,&
                    QuadSc%valU,QuadSc%valV,QuadSc%valW,&
@@ -2812,3 +2814,22 @@ END DO
 mySetup%bPressureConvergence = mySetup%bPressureConvergence.and.(istart.eq.1)
 
 END SUBROUTINE GetPressureAtInflows
+
+
+!! Called by all (myid=0 too) processes because of the immersed communicated sum !!
+SUBROUTINE IntegrateShearrate()
+REAL*8   dAvgShearRate
+EXTERNAL E013
+
+ILEV = NLMAX
+CALL SETLEV(2)
+
+CALL SubIntegrateShearrate(QuadSc%valU,QuadSc%valV,QuadSc%valW,&
+                        mg_mesh%level(ILEV)%kvert,&
+                        mg_mesh%level(ILEV)%karea,&
+                        mg_mesh%level(ILEV)%kedge,&
+                        mg_mesh%level(ILEV)%dcorvg,&
+                        dAvgShearRate,&
+                        E013)
+
+END SUBROUTINE IntegrateShearrate
