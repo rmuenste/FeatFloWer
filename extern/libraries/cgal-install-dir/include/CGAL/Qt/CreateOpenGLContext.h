a8e53a75c7d3c15470231a2a1c4c7f48dd64c597
Raphael Münster <raphael.muenster@tu-dortmund.de>
2023-12-12 11:44:56 +0100

    Including cgal as source and some CMake changes

---
+++ extern/libraries/cgal-install-dir/include/CGAL/Qt/CreateOpenGLContext.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2015  GeometryFactory SARL (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org)
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: LGPL-3.0-or-later OR LicenseRef-Commercial
+//
+//
+// Author(s)     : Laurent Rineau and Maxime Gimeno
+#ifndef CGAL_QT_CREATE_OPENGL_CONTEXT_H
+#define CGAL_QT_CREATE_OPENGL_CONTEXT_H
+
+#include <QOpenGLContext>
+
+namespace CGAL{
+namespace Qt{
+inline QOpenGLContext* createOpenGLContext()
+{
+    QOpenGLContext *context = new QOpenGLContext();
+    QSurfaceFormat format;
+    format.setVersion(2,1);
+    format.setProfile(QSurfaceFormat::CompatibilityProfile);
+    context->setFormat(format);
+    context->create();
+    return context;
+}
+} // namespace Qt
+} // namespace CGAL
+#endif
--- extern/libraries/lapack-3.6.1/CMAKE/CheckTimeFunction.cmake
+++ extern/libraries/lapack-3.6.1/CMAKE/CheckTimeFunction.cmake
@@ -7,6 +7,8 @@
 
 macro(CHECK_TIME_FUNCTION FUNCTION VARIABLE)
 
+    set(OUTPUT "")  # Initialize OUTPUT_VARIABLE to an empty string
+
     try_compile(RES
     ${PROJECT_BINARY_DIR}/INSTALL
     ${PROJECT_SOURCE_DIR}/INSTALL
@@ -14,14 +16,17 @@ macro(CHECK_TIME_FUNCTION FUNCTION VARIABLE)
     OUTPUT_VARIABLE OUTPUT)
 
     if(RES)
-      set(${VARIABLE} ${FUNCTION} CACHE INTERNAL "Have Fortran function ${FUNCTION}")
-      message(STATUS "Looking for Fortran ${FUNCTION} - found")
+      #set(${VARIABLE} ${FUNCTION} CACHE INTERNAL "Have Fortran function ${FUNCTION}")
+      set(${VARIABLE} ${FUNCTION})
+      message(STATUS "Looking for Fortran ${FUNCTION} - found - function name: ${VARIABLE} ${FUNCTION}")
       file(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeOutput.log
         "Fortran ${FUNCTION} exists. ${OUTPUT} \n\n")
+      message(STATUS "Fortran ${FUNCTION} exists. ${OUTPUT}")
     else(RES)
       message(STATUS "Looking for Fortran ${FUNCTION} - not found")
       file(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log
         "Fortran ${FUNCTION} does not exist. \n ${OUTPUT} \n")
+      message(STATUS "Fortran ${FUNCTION} does not exist. ${OUTPUT}")
     endif(RES)
 endmacro(CHECK_TIME_FUNCTION)
 
