393e75fd8b0273d62c10ba43b350df6e6b5e1e59
superman <raphael.muenster@math.tu-dortmund.de>
2025-12-04 12:05:05 +0100

    Adjusted the dLubricationFactor for a smaller mesh size

--- applications/q2p1_bench_sedimentation/app_init.f90
+++ applications/q2p1_bench_sedimentation/app_init.f90
@@ -83,7 +83,7 @@ SUBROUTINE set_lubrication_threshold_from_mesh(myid)
 #ifdef HAVE_PE
   ! Configuration parameter: lubrication threshold = factor * h_min
   ! User can modify this value based on their simulation requirements
-  dLubricationFactor = 4.0d0  ! Default: 3x minimum mesh size
+  dLubricationFactor = 5.0d0  ! Default: 3x minimum mesh size
 
   ! Get mesh data structures
   NEL = mg_Mesh%level(NLMAX)%nel
