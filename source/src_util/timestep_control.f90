2103b57e6e4b667619cc396d04f5eb1b8af2717b
superman <raphael.muenster@math.tu-dortmund.de>
2026-03-09 10:42:13 +0100

    Extended the time control interface

--- source/src_util/timestep_control.f90
+++ source/src_util/timestep_control.f90
@@ -3,7 +3,7 @@ MODULE timestep_control
   IMPLICIT NONE
 
   PRIVATE
-  PUBLIC :: SetSimulationTimeStep
+  PUBLIC :: SetSimulationTimeStep, GetSimulationTimeStep
 
 CONTAINS
 
@@ -32,5 +32,30 @@ CONTAINS
     CALL set_pe_timestep(TSTEP)
 #endif
   END SUBROUTINE SetSimulationTimeStep
+
+  SUBROUTINE GetSimulationTimeStep(dTimeStep)
+    IMPLICIT NONE
+
+    REAL*8, INTENT(OUT) :: dTimeStep
+
+    DOUBLE PRECISION :: TSTEP, THETA, THSTEP, TIMENS, EPSNS
+    INTEGER :: NITNS, ITNS
+    COMMON /NSPAR/  TSTEP,THETA,THSTEP,TIMENS,EPSNS,NITNS,ITNS
+
+#ifdef HAVE_PE
+    INTERFACE
+      SUBROUTINE get_pe_timestep(dTime) BIND(C, name="get_pe_timestep_")
+        USE, INTRINSIC :: iso_c_binding, ONLY: c_double
+        REAL(c_double), INTENT(OUT) :: dTime
+      END SUBROUTINE get_pe_timestep
+    END INTERFACE
+#endif
+
+#ifdef HAVE_PE
+    CALL get_pe_timestep(dTimeStep)
+#else
+    dTimeStep = TSTEP
+#endif
+  END SUBROUTINE GetSimulationTimeStep
 
 END MODULE timestep_control
